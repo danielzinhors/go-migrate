1d55ca9243d31d61bbe342b30543628442bc8dc6
danielzinhors <danielzinhors@gmail.com>
2024-01-08 20:57:22 -0300

    craido a criaçao e a listagem das categorias

--- sql/queries/query.sql
+++ sql/queries/query.sql
@@ -1,2 +1,18 @@
 -- name: ListCategories :many
 SELECT * FROM categories;
+
+-- name: GetCategory :one
+SELECT * FROM categories
+where ID = ?;
+
+-- name: CreateCategory :exec
+INSERT INTO categories (id, name, description) VALUES (?, ?, ?);
+
+
+-- name: UpdateCategory :exec
+UPDATE categories SET name = ?, description = ?
+where id = ?;
+
+-- name: DeleteCategory :exec
+DELETE FROM categories
+WHERE id = ?;
