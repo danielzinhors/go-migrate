285654d7ce0be793a450ee222eb73711fc80157d
danielzinhors <danielzinhors@gmail.com>
2024-01-08 21:02:48 -0300

    criado o crud

--- cmd/runSQLC/main.go
+++ cmd/runSQLC/main.go
@@ -6,7 +6,6 @@ import (
 
 	"github.com/danielzinhors/go-migrate/internal/db"
 	_ "github.com/go-sql-driver/mysql"
-	"github.com/google/uuid"
 )
 
 func main() {
@@ -17,17 +16,50 @@ func main() {
 	}
 	defer dbConn.Close()
 	queries := db.New(dbConn)
-	err = queries.CreateCategory(ctx, db.CreateCategoryParams{
-		ID:          uuid.New().String(),
-		Name:        "Backend",
-		Description: sql.NullString{String: "Backend description", Valid: true},
-	})
+	// err = queries.CreateCategory(ctx, db.CreateCategoryParams{
+	// 	ID:          uuid.New().String(),
+	// 	Name:        "Backend",
+	// 	Description: sql.NullString{String: "Backend description", Valid: true},
+	// })
 
+	// if err != nil {
+	// 	panic(err)
+	// }
+
+	// categories, err := queries.ListCategories(ctx)
+	// if err != nil {
+	// 	panic(err)
+	// }
+
+	// for _, category := range categories {
+	// 	println(category.ID, category.Name, category.Description.String)
+	// }
+	// err = queries.UpdateCategory(ctx, db.UpdateCategoryParams{
+	// 	ID:          "fec2f257-a066-4dca-be5e-3ae23857a821",
+	// 	Name:        "Backend Update",
+	// 	Description: sql.NullString{String: "Backend description update", Valid: true},
+	// })
+	// if err != nil {
+	// 	panic(err)
+	// }
+
+	// categories, err := queries.ListCategories(ctx)
+	// if err != nil {
+	// 	panic(err)
+	// }
+	// if err != nil {
+	// 	panic(err)
+	// }
+
+	// for _, category := range categories {
+	// 	println(category.ID, category.Name, category.Description.String)
+	// }
+
+	err = queries.DeleteCategory(ctx, "fec2f257-a066-4dca-be5e-3ae23857a821")
+	categories, err := queries.ListCategories(ctx)
 	if err != nil {
 		panic(err)
 	}
-
-	categories, err := queries.ListCategories(ctx)
 	if err != nil {
 		panic(err)
 	}
